a6907edadd15d92fccdb71aa24d4d0f2ea11a90a
Shubham <shubhamjadhav0806@gmail.com>
2025-07-27 11:13:38 +0530

    Task 3: Added comments

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,13 +1,15 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require('mysql2');    // Import mysql2 module
+require('dotenv').config();         // Load environment variables
 
+// Set up the config
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST,      // Read from .env file
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
+// Actually Connect to MySQL
 db.connect((err) => {
     if (err) {
         console.error("Error connection to MySql: ", err);
@@ -16,4 +18,4 @@ db.connect((err) => {
     console.log("Connected to MySql Database");
 });
 
-module.exports = db;
+module.exports = db;    // export the connection so other files (like routes) can use it.
--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const db = require('./db/connection');
+const express = require('express'); // core web server
+const cors = require('cors');       // Allows React frontend to talk to backend (Cross origin resource sharing)
+require('dotenv').config();         // load .env variables
+const db = require('./db/connection'); // import db connection
 
-const app = express();
+const app = express();  // create express ap p
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors());        // enable cors
+app.use(express.json());    // parse json bodies from post request
 
 // Root route for testing
 app.get('/', (req, res) => {
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // if process.env.PORT is not set (undefined) then use 5000 as a default
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
